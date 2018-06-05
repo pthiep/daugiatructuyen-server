0c949f312dedb1067a4c219c07d888e007afd9a3
NguyenHoang263 <ntthoai1997@gmail.com>
2018-06-05 22:28:54 +0700

    create database

--- api/repository/productRespository.js
+++ api/repository/productRespository.js
@@ -6,7 +6,19 @@ exports.loadAll = function() {
 }
 
 // Top 5 sản phẩm có nhiều lượt ra giá nhất
+exports.loadAll = function(){
+	var sql = 'select * from mathang order by soluottc desc limit 5';
+	return db.load(sql);
+}
 
 // Top 5 sản phẩm có giá cao nhất
+exports.loadAll = function(){
+	var sql = 'select * from mathang order by giahang desc limit 5';
+	return db.load(sql);
+}
 
-// Top 5 sản phẩm gần kết thúc
+// Top 5 sản phẩm gần kết thúcẽ
+exports.loadAll = function(){
+	var sql = 'select * from mathang order by DATEDIFF(NGAYKETTHUC,NGAYDAT) desc limit 5';
+	return db.load(sql);
+}
